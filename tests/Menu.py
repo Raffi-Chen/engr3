a6b8aceedcb321dcb9cf6b33ae3a2bf8227d77a7
Raffi-Chen <rchen41@charlottesvilleschools.org>
2024-01-18 15:34:39 -0500

    Stop/caution/go test

--- tests/Menu.py
+++ tests/Menu.py
@@ -1,17 +1,24 @@
 import rotaryio
 import board
+import time
 
 menu = ["stop", "caution", "go"]
 
 last_index = None
 menu_index = 0
 
+
+
 enc = rotaryio.IncrementalEncoder(board.D4, board.D3, divisor=2)
 
 while True:
 
     menu_index = enc.position
+    menu_index_lcd = menu_index % 3
+    print(menu[menu_index_lcd])
 
     if (last_index is None) or (menu_index is not 0):
-        print(menu_index)
+        #print(menu_index_lcd)
         last_index = menu_index
+
+    time.sleep(1)
