1e798ab8d507959f45c263672656b4696afc9e5e
Raffi-Chen <rchen41@charlottesvilleschools.org>
2024-01-18 15:59:23 -0500

    Almost done w/ Rotary encoder!

--- Rotary Encoder.py
+++ Rotary Encoder.py
@@ -4,7 +4,7 @@ import neopixel
 import digitalio
 from lcd.lcd import LCD # importing lcd stuff below
 from lcd.i2c_pcf8574_interface import I2CPCF8574Interface
-import busio
+#import busio
 import time
 
 enc = rotaryio.IncrementalEncoder(board.D4, board.D3, divisor=2)
@@ -22,14 +22,49 @@ button.direction = digitalio.Direction.INPUT # Establishes button input
 button.pull = digitalio.Pull.UP
 button_state = None
 
-lcd.print("!23123") # Prints value to LCD screen
+menu = ["stop", "caution", "go"]
+
+last_index = None
+menu_index = 0
+menu_index = enc.position
+menu_index_lcd = menu_index % 3
 
 while True:
+ #   print(menu[menu_index_lcd])
+    if button.value and button_state == "pressed":
 
-    led[0] = (255, 0, 0) # Projects certain color to LED
+        if menu_index is 0:
+
+            led[0] = (255, 0, 0)
+            print(menu[menu_index_lcd])
+            lcd.print(menu[menu_index_lcd])
+
+        if menu_index is 1:
+
+            led[0] = (255, 255, 0)
+            print(menu[menu_index_lcd])
+            lcd.print(menu[menu_index_lcd])
+
+        if menu_index is 2:
+
+            led[0] = (0, 255, 0)
+            print(menu[menu_index_lcd])
+            lcd.print(menu[menu_index_lcd])
+
+#-------------------------------------
+
+'''    led[0] = (255, 0, 0) # Projects certain color to LED
 
     if not button.value and button_state is None: # If button value is NOT 0 (if it is 1/pressed)
         button_state = "pressed"
     if button.value and button_state == "pressed":
         print("Button is pressed")
         button_state = None
+
+    print(menu[menu_index_lcd])
+
+    if (last_index is None) or (menu_index is not 0):
+        #print(menu_index_lcd)
+        last_index = menu_index
+
+    time.sleep(1)'''
