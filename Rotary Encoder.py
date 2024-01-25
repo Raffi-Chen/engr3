fb03ef4d23c79bd6ab1d2acf14e9cbf943a9dd3a
Raffi-Chen <rchen41@charlottesvilleschools.org>
2024-01-25 15:27:45 -0500

    Added commenting

--- Rotary Encoder.py
+++ Rotary Encoder.py
@@ -1,18 +1,17 @@
-import rotaryio
-import board
-import neopixel
-import digitalio
+import rotaryio # Imports value of rotary encoder's turning function to use later
+import board # Allows code to relate to certain pins on board
+import neopixel # Activates board's neopixel for later use
+import digitalio # For use in button (digital values are 1/0 rather than fluid)
 from lcd.lcd import LCD # importing lcd stuff below
 from lcd.i2c_pcf8574_interface import I2CPCF8574Interface
 
-enc = rotaryio.IncrementalEncoder(board.D4, board.D3, divisor=2)
+enc = rotaryio.IncrementalEncoder(board.D4, board.D3, divisor=2) # Establishes pins of rotary encoder
 
 lcd = LCD(I2CPCF8574Interface(board.I2C(),0x27),num_rows=2,num_cols=16) 
 # Begin communication with LCD screen
-#i2c = busio.I2C(board.SCL,board.SDA)
 
 led = neopixel.NeoPixel(board.NEOPIXEL,1)
-led.brightness = 0.05
+led.brightness = 0.1
 # Establishes properties of neopixel
 
 button = digitalio.DigitalInOut(board.D2) # Establishes communication with 2nd pin
@@ -20,22 +19,23 @@ button.direction = digitalio.Direction.INPUT # Establishes button input
 button.pull = digitalio.Pull.UP
 button_state = 0
 
-menu = ["stop         ", "caution         ", "go          "]
+menu = ["stop         ", "caution         ", "go          "] # Used for menu later
 
-last_index = None
 menu_index = 0
 
 while True:
 
-    menu_index = enc.position
-    menu_index_lcd = menu_index % 3
+    menu_index = enc.position # Menu index is what encoder reads
+    menu_index_lcd = menu_index % 3 # Finds remainder of # to read only 1, 2, or 3
 
     lcd.set_cursor_pos(0,0)
-    lcd.print(menu[menu_index_lcd])
+    lcd.print(menu[menu_index_lcd]) # Initiates + displays LCD values "stop", "caution", "go"
 
     if button.value and button_state == 0:
-        print(f"Menu Index = {menu_index_lcd}")
+
+        print(f"Menu Index = {menu_index_lcd}") # F string allows us to print both text and variable to describe state of menu index with text
         button_state = 1
+
         if menu_index_lcd is 0:
 
             led[0] = (255, 0, 0)
@@ -46,25 +46,8 @@ while True:
 
         if menu_index_lcd is 2:
 
-            led[0] = (0, 255, 0)
+            led[0] = (0, 255, 0) # If then to tell neopixel what to show
 
     elif not button.value:
-        button_state = 0
 
-#-------------------------------------
-
-'''    led[0] = (255, 0, 0) # Projects certain color to LED
-
-    if not button.value and button_state is None: # If button value is NOT 0 (if it is 1/pressed)
-        button_state = "pressed"
-    if button.value and button_state == "pressed":
-        print("Button is pressed")
-        button_state = None
-
-    print(menu[menu_index_lcd])
-
-    if (last_index is None) or (menu_index is not 0):
-        #print(menu_index_lcd)
-        last_index = menu_index
-
-    time.sleep(1)'''
+        button_state = 0 # Button is not pressed if not pressed
