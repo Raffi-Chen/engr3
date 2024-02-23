e635e4ad113b76ba36532be72614b4dae414b748
Raffi Chen <143544930+Raffi-Chen@users.noreply.github.com>
2024-02-23 15:40:40 -0500

    Added stepper motor commenting

--- StepperMotors.py
+++ StepperMotors.py
@@ -1,14 +1,14 @@
-import asyncio
+import asyncio # Initiates "fake" multi-threading in order to "do" multiple things at the same time
 import board
-import keypad
+import keypad # communicate with limit switch
 import time
-import digitalio
-from adafruit_motor import stepper
+import digitalio # 
+from adafruit_motor import stepper # imports stepper motor
 
 DELAY = 0.01
 STEPS = 100
 
-coils = (
+coils = ( # Distributes stepper motor to multiple centers for power
     digitalio.DigitalInOut(board.D9),  # A1
     digitalio.DigitalInOut(board.D10), # A2
     digitalio.DigitalInOut(board.D11), # B1
@@ -20,7 +20,7 @@ for coil in coils:
 
 motor = stepper.StepperMotor(coils[0], coils[1], coils[2], coils[3], microsteps=None)
  
-async def catch_pin_transitions(pin):
+async def catch_pin_transitions(pin): # Loop 1 responds to limit switch when pressed
     # Print a message when pin goes low and when it goes high.
     with keypad.Keys((pin,), value_when_pressed=False) as keys:
         while True:
@@ -29,25 +29,24 @@ async def catch_pin_transitions(pin):
                 if event.pressed:
                     print("Limit Switch was pressed.")
                     for step in range(STEPS):
-                        motor.onestep(direction=stepper.BACKWARD, style=stepper.DOUBLE)
+                        motor.onestep(direction=stepper.BACKWARD, style=stepper.DOUBLE) # Tells stepper motor to move backwards
                         time.sleep(DELAY) 
-                elif event.released:
+                elif event.released: # when released
                     print("Limit Switch was released.")
-            await asyncio.sleep(0)
+            await asyncio.sleep(0) # Tell asyncio to go to the next event immediately
 
-async def run_motor():
+async def run_motor(): # Constant limit switch loop
     while(True):
         for step in range(STEPS):
             motor.onestep(style=stepper.DOUBLE)
             time.sleep(DELAY)
         await asyncio.sleep(0)
 
-async def main():
+async def main(): # in this loop, both async functions are defined as new asyncio tasks
     while(True):
         interrupt_task = asyncio.create_task(catch_pin_transitions(board.D2))
-        # USE asyncio.create_task() to call the run_motor() function you wrote.
         motor_task = asyncio.create_task(run_motor())
 
         await asyncio.gather(interrupt_task, motor_task)
 
-asyncio.run(main())
+asyncio.run(main()) # Finalizes asyncio run
